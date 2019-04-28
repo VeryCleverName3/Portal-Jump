35c84abc7afdea35f6c3e962755e60e78eca6cd5
VeryCleverName3 <38430676+VeryCleverName3@users.noreply.github.com>
2019-04-28 09:29:29 -0400

    Update main.js

--- main.js
+++ main.js
@@ -64,6 +64,9 @@ function antiCheat() {
 			localStorage.highscore = highscore;
 		}
 	}, 1);
+	if(!localStorage.mobile){
+		document.body.style.zoom = window.innerHeight / 800;	
+	}
 	var c = document.getElementById("gameCanvas");
 	var winPlayer = new Image();
 	winPlayer.src = "winPlayer.png";
